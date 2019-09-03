7e91083d96990d076b971771f2ff1c24aa168fa7
Niclas Binninge <alltjagvill@gmail.com>
2019-09-03 16:12:14 +0200

    Started project

---
+++ Game N watch - Helmet/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInput : MonoBehaviour
+{
+    //Skapar en "lista" på vad som kan hända.
+    public enum Button
+    {
+        left,
+        right
+    }
+
+
+    //Initierar en ButtonInput
+    public Button button;
+
+    //För att att skicka ut ett event måste vi skapa en delegate
+    public delegate void ButtonPress();
+    
+
+    //Här skapas sjäklva eventsen som kan hända
+    public static ButtonPress PressLeft;
+    public static ButtonPress PressRight;
+
+        private void OnMousDown()
+        {
+            
+            if (PressLeft != null && button == Button.left) //Kollar att det faktiskt är någon som premunerar på eventet
+            {
+                PressLeft();
+            }
+
+            else if (PressRight != null && button == Button.right) //Kollar att det faktiskt är någon som premunerar på eventet
+            {
+                PressRight();
+            }
+
+        }
+    
+}
---
+++ Game N watch - Helmet/Assets/Scripts/HelmetController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelmetController : MonoBehaviour
+{
+    public List<GameObject> positions = new List<GameObject>();
+
+    Vector3 startPosition;
+    
+    //private float startPostition = position[0.];
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = positions[0].transform.position;
+        GoToStart();
+    }
+
+    // Update is called once per frame
+
+
+    private void GoToStart()
+    {
+        
+        if (positions != null)
+        {
+        transform.position = startPosition; 
+        Debug.Log("Position: " + startPosition);
+        Debug.Log("Number in list: " + positions.Count);
+        }
+    }
+    
+
+
+}
